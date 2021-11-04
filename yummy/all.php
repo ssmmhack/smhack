addbc695ce247d0e58f843c058a03fe04bd89649
SuminLim0 <81598640+SuminLim0@users.noreply.github.com>
2021-11-04 15:00:35 +0900

    Add files via upload

---
+++ yummy/all.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<?php
+function print_title(){
+    if(isset($_GET['id'])){
+        echo $_GET['id'];
+    }
+    else{
+        echo "Welcome";
+    }
+}
+function print_description(){
+    if(isset($_GET['id'])){
+    echo file_get_contents("data/".$_GET['id']);
+    }
+    else{
+        echo "Hello, PHP";
+        }
+    }
+function print_image(){
+    if(isset($_GET['id'])){
+        $image = $_GET['id'];
+        echo "<img src='$image.jpg' width= 90%>";
+    }
+    else echo "<img src='all.jpg' width=90%>";
+}
+    
+    
+function print_list(){
+    $list = scandir('./data');
+
+        $i=0;
+        while($i < count($list)){
+            if($list[$i] != '.'){
+                if ($list[$i] != '..'){
+                    echo "<li><a href =\"all.php?id=$list[$i]\">$list[$i]</a></li>\n";
+                }
+            }
+            $i = $i +1;
+        }
+}
+?>
+<html lang="en">
+<head>
+    
+    <link rel="stylesheet" href="allstyle.css">
+</head>
+<body>
+    <h3><u><a href="all.php">ALL</a></u></h3>
+    <div id="choose">
+        메뉴를 선택하면 상세정보를 볼 수 있어요~
+    </div>
+    <div id="grid">
+        <ol>
+            <?php
+            print_list();
+            ?>
+        </ol>
+        <div id= "ingrid">
+            <h5>
+                <?php
+                print_title();
+                ?>
+            </h5>
+            
+            <?php
+                print_image(); ?>
+        </div>
+        <div id="des">
+            <?
+            print_description();
+            ?>
+        </div>
+    </div>
+    <a href="create.php">create</a>
+    <?php if(isset($_GET['id'])){ ?>
+        <a href="update.php?id=<?=$_GET['id']; ?>">update</a>
+        <a href="delete_process.php?id=<?=$_GET['id']; ?>">delete</a>
+        <form action="delete_process.php" method="post">
+            <input type="hidde" name="id" value="<?=$_GET['id']?>">
+            <input type="submit" value="delete">
+        </form>
+    <?php } ?>
+</body>
+</html>
---
+++ yummy/create.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<?php
+function print_title(){
+    if(isset($_GET['id'])){
+        echo $_GET['id'];
+    }
+    else{
+        echo "Welcome";
+    }
+}
+function print_description(){
+    if(isset($_GET['id'])){
+    echo file_get_contents("data/".$_GET['id']);
+    }
+    else{
+        echo "Hello, PHP";
+        }
+    }
+function print_image(){
+    if(isset($_GET['id'])){
+        $image = $_GET['id'];
+        echo "<img src='$image.jpg' width= 90%>";
+    }
+    else echo "<img src='all.jpg' width=90%>";
+}
+    
+    
+function print_list(){
+    $list = scandir('./data');
+
+        $i=0;
+        while($i < count($list)){
+            if($list[$i] != '.'){
+                if ($list[$i] != '..'){
+                    echo "<li><a href =\"all.php?id=$list[$i]\">$list[$i]</a></li>\n";
+                }
+            }
+            $i = $i +1;
+        }
+}
+?>
+<html lang="en">
+<head>
+    
+    <link rel="stylesheet" href="allstyle.css">
+</head>
+<body>
+    <h3><u><a href="all.php">ALL</a></u></h3>
+    <div id="choose">
+        메뉴를 선택하면 상세정보를 볼 수 있어요~
+    </div>
+    <div id="grid">
+        <ol>
+            <?php
+            print_list();
+            ?>
+        </ol>
+        <div id= "ingrid">
+            <h5>
+                <?php
+                print_title();
+                ?>
+            </h5>
+            
+            <?php
+                print_image(); ?>
+        </div>
+        <div id="des">
+            <?
+            print_description();
+            ?>
+        </div>
+    </div>
+    <form action="create_process.php" method = "post">
+        <p>
+            <input type="text" name="title"
+            placeholder="Title">
+        </p>
+        <p>
+            <textarea name="description" palceholder="Description">
+            </textarea>
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+    </form>
+
+</body>
+</html>

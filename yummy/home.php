c40f5ba0b05f5912224d2b67c8dede0f26d30fce
honi20 <81567670+honi20@users.noreply.github.com>
2021-11-04 15:05:33 +0900

    Create home.php

---
+++ yummy/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Yummy</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="bbb.css">
+    </head>
+
+    <body>
+        <div id="login"><a href="login.html"><img src="login.png" height=30 width=30></a></div>
+        <h1 id="every">Yummy</h1>
+        <table bgcolor="pink">
+            <tr>
+                <td><a href="all.php" height=20 width=20 target="_self"><img src="all.png" height=100 width=100></a></td> 
+                <td><a href="aaa.html" height=20 width=20 target="_self"><img src="america.jpg" height=100 width=100></a></td> 
+                <td><a href="aaa.html" height=20 width=20 target="_self"><img src="ja.jpg" height=100 width=100></a></td> 
+            </tr>
+            <tr>
+                <td><a href="aaa.html" height=20 width=20 target="_self"><img src="korean.jpg" height=100 width=100></a></td> 
+                <td><a href="aaa.html" height=20 width=20 target="_self"><img src="chi.png" height=100 width=100></a></td> 
+                <td><a href="aaa.html" height=20 width=20 target="_self"><img src="des.jpg" height=100 width=100></a></td> 
+            </tr>
+
+        </table>
+
+
+
+    </body>
+</html>
